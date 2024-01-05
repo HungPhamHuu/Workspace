90e0f1895721ca257771feab638810ebaaa3802e
HungPhamHuu <hungtip2408@gmail.com>
2024-01-05 14:49:13 +0700

    a

--- Html_Css/Hinh2/main.js
+++ Html_Css/Hinh2/main.js
@@ -1,15 +1,13 @@
-
-function checkCustomer() {
-    var ma_kh = document.getElementById("ma_kh").value;
+var ma_kh = document.getElementById("ma_kh");
+ma_kh.addEventListener("blur", function checkCustomer(){
     const custommers = {
         "KH001": { "ten_kh": "Khách hàng 1", "dia_chi": "Hà Nội" }
     };
     if (custommers[ma_kh]) {
-        document.getElementById("ten_kh").textContent = custommers[ma_kh].ten_kh;
-        document.getElementById("dia_chi").textContent = custommers[ma_kh].dia_chi;
+        document.getElementById("ten_kh").value = custommers[ma_kh].ten_kh;
+        document.getElementById("dia_chi").value = custommers[ma_kh].dia_chi;
     } else {
-         alert("Mã khách hàng không tồn tại");
+        alert("Mã khách hàng không tồn tại");
     }
+});
 
-}
-document.getElementById("ma_kh").addEventListener("input", checkCustomer);
