6b81d1fb66dba5aab6a26685f19c86724b269a09
HungPhamHuu <hungtip2408@gmail.com>
2024-01-10 15:33:30 +0700

    done

--- Html_Css/Hinh1/main.js
+++ Html_Css/Hinh1/main.js
@@ -1,3 +1,72 @@
 function redirectToHinh2(){
+    window.location.href = "../Hinh2/index.html";
+}
+
+function XoaDuLieuCu(){
+    localStorage.removeItem("editData");
+}
+
+document.addEventListener("DOMContentLoaded", function (){
+    XoaDuLieuCu();
+    var saveData = localStorage.getItem("saveData");
+    if(saveData){
+        var parseData = JSON.parse(saveData);
+        // var tableRow = document.getElementById("tbody");
+
+        document.getElementById("ngay_lap").textContent = parseData.ngayLap || "";
+        document.getElementById("so_phieu").textContent = parseData.soPhieu || "";
+        document.getElementById("ma_kh").textContent = parseData.maKH || "";
+        document.getElementById("ten_kh").textContent = parseData.tenKH || "";
+        document.getElementById("t_tien").textContent = parseData.thanhTien || "";
+        document.getElementById("t_ck").textContent = parseData.chietKhau || "";
+        document.getElementById("t_thue").textContent = parseData.thue || "";
+        document.getElementById("t_phai_thu").textContent = parseData.tongPhaiThu || "";
+        document.getElementById("trang_thai").textContent = parseData.trangThai || "";
+    }
+});
+
+function suaForm(){
+    var ngayLapElement = document.getElementById("ngay_lap");
+    console.log(ngayLapElement);
+    var ngayLap = ngayLapElement ? ngayLapElement.textContent : "";
+    var soPhieuElement = document.getElementById("so_phieu");
+    console.log(soPhieuElement);
+    var soPhieu = soPhieuElement ? soPhieuElement.textContent : "";
+    var makhElement = document.getElementById("ma_kh");
+    console.log(makhElement);
+    var makh = makhElement ? makhElement.textContent : "";
+    var tenkhElement = document.getElementById("ten_kh");
+    console.log(tenkhElement);
+    var tenkh = tenkhElement ? tenkhElement.textContent : "";
+    var tongtienElement = document.getElementById("t_tien");
+    console.log(tongtienElement);
+    var tongtien = tongtienElement ? tongtienElement.textContent : "";
+    var tongckElement = document.getElementById("t_ck");
+    console.log(tongckElement);
+    var tongCk = tongckElement ? tongckElement.textContent : "";
+    var tongthueElement = document.getElementById("t_thue");
+    console.log(tongthueElement);
+    var tongthue = tongthueElement ? tongthueElement.textContent : "";
+    var tongphaithuElement = document.getElementById("t_phai_thu");
+    console.log(tongphaithuElement);
+    var tongphaithu = tongphaithuElement ? tongphaithuElement.textContent : "";
+    var trangthaiElement = document.getElementById("trang_thai");
+    console.log(trangthaiElement);
+    var trangthai = trangthaiElement ? trangthaiElement.textContent : "";
+
+    var editData = {
+        ngayLap: ngayLap,
+        soPhieu: soPhieu,
+        makh: makh,
+        tenkh: tenkh,
+        tongtien: tongtien,
+        tongCk: tongCk,
+        tongthue: tongthue,
+        tongphaithu: tongphaithu,
+        trangthai: trangthai
+    };
+
+    localStorage.setItem("editData", JSON.stringify(editData));
+
     window.location.href = "../Hinh2/index.html"
 }
--- Html_Css/Hinh2/main.js
+++ Html_Css/Hinh2/main.js
@@ -30,37 +30,150 @@ ma_nv.addEventListener("change", function checkStaff() {
     }
 });
 
-// function getProductByIMEI(imei_input){
-//     const produts = [
-//         {imei: "IP12PRX0001", gia: 12000000, ma_vt: "IP12PRX", ten_vt: "Iphone", dvt: "Chiếc", so_luong: 1, thue_suat: 10},
-//         {imei: "IP12PRX0002", gia: 12000000, ma_vt: "IP12PRX", ten_vt: "Iphone", dvt: "Chiếc", so_luong: 1, thue_suat: 10}
-//     ];
-//     return produts.find(products => products.imei === imei_input);
-// }
+function updateTable(index){
+    var table = document.getElementById("tableTbody");
+    var newRow = table.insertRow();
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
+    var cell6 = newRow.insertCell(5);
+    var cell7 = newRow.insertCell(6);
+    var cell8 = newRow.insertCell(7);
+    var cell9 = newRow.insertCell(8);
+    var cell10 = newRow.insertCell(9);
+
+    var tien_thue = index.gia * index.thue_suat / 100;
+    var tien = index.gia + tien_thue;
+
+    cell1.textContent = index.ma_vt;
+    cell2.textContent = index.ten_vt;
+    cell3.textContent = index.imei;
+    cell4.textContent = index.dvt;
+    cell5.textContent = index.gia;
+    cell6.textContent = index.so_luong;
+    cell7.textContent = index.thanh_tien;
+    cell8.textContent = index.thue_suat;
+    cell9.textContent = tien_thue;
+    cell10.textContent = tien;
+}
+function updateSumValues(){
+    var sumSoLuong = totalOrder(5);
+    var sumGia = totalOrder(4);
+    var sumThue = totalOrder(8);
+
+    tSoLuongInput.value = sumSoLuong;
+    tTienInput.value = sumGia;
+    tThueInput.value = sumThue;
+
+    var tPhaiThu = sumGia + sumThue - parseFloat(tienCocInput.value || 0);
+    tPhaiThuInput.value = tPhaiThu;
+}
+
+function totalOrder(columnIndex){
+    var sum = 0;
+    var rows = listTableBody.getElementsByTagName("tr");
+
+    for(var i = 0; i < rows.length; i++){
+        var cell = rows[i].getElementsByTagName("td")[columnIndex];
+        if(cell){
+            sum += parseFloat(cell.textContent) || 0
+        }
+    }
+    return sum;
+    
+}
 
 var imei = document.getElementById("imei");
+var listTableBody = document.getElementById("tableTbody");
+var tSoLuongInput = document.getElementById("t_so_luong");
+var tTienInput = document.getElementById("t_tien");
+var tThueInput = document.getElementById("t_thue");
+var tPhaiThuInput = document.getElementById("t_phai_thu");
+var tienCocInput = document.getElementById("tien_coc");
+var imeiCount = 0;
 imei.addEventListener("change", function checkImei() {
     const produts = [
-        { imei: "IP12PRX0001", gia: 12000000, ma_vt: "IP12PRX1", ten_vt: "Iphone", dvt: "Chiếc", so_luong: 1, thue_suat: 10 },
-        { imei: "IP12PRX0002", gia: 12000000, ma_vt: "IP12PRX2", ten_vt: "SamSung", dvt: "Chiếc", so_luong: 1, thue_suat: 10 }
+        { imei: "IP12PRX0001", gia: 12000000, ma_vt: "IP12PRX1", ten_vt: "Iphone", dvt: "Chiếc", so_luong: 1, thue_suat: 10, thanh_tien: 12000000 },
+        { imei: "IP12PRX0002", gia: 12000000, ma_vt: "IP12PRX2", ten_vt: "SamSung", dvt: "Chiếc", so_luong: 1, thue_suat: 10, thanh_tien: 12000000 }
     ];
-    const imei_input = imei.value;
-    const selectImei = produts.find(produts => produts.imei === imei_input);
+    var imei_input = imei.value;
+    var selectImei = produts.find(produts => produts.imei === imei_input);
+    if(imei_input !== ""){
+        if (selectImei) {
 
-    if (selectImei) {
-        var tien_thue = selectImei.gia * selectImei.thue_suat / 100;
-        var tien = selectImei.gia + tien_thue;
-
-        document.getElementById("gia").textContent = selectImei.gia;
-        document.getElementById("ma_vt").textContent = selectImei.ma_vt;
-        document.getElementById("ten_vt").textContent = selectImei.ten_vt;
-        document.getElementById("dvt").textContent = selectImei.dvt;
-        document.getElementById("so_luong").textContent = selectImei.so_luong;
-        document.getElementById("thue_suat").textContent = selectImei.thue_suat;
-        document.getElementById("tien_thue").textContent = tien_thue;
-        document.getElementById("tien").textContent = tien;
-    } else {
-        alert("Mã IMEI không tồn tại");
+            updateTable(selectImei);
+            imeiCount++;
+            if(imeiCount === 2){
+                updateSumValues();
+            }      
+        } else {
+            alert("Mã IMEI không tồn tại");
+        }
     }
 });
+
+function isValidInput(){
+    var soPhieu = document.getElementById("so_phieu").value;
+    var ngayLap = document.getElementById("ngay_lap").value;
+    var tienCoc = document.getElementById("tien_coc").value;
+    
+    if(!soPhieu || !ngayLap || isNaN(parseFloat(tienCoc))){
+        alert("Vui lòng nhập đúng thông tin và định dạng");
+        return false;
+    }
+
+    return true;
+}
+var button = document.getElementById("themMoi")
+button.addEventListener("click", function saveLocalStorage() {
+    if(!isValidInput()){
+        return ;
+    }
+
+    var dataToSave = {
+        soPhieu: document.getElementById("so_phieu").value,
+        ngayLap: document.getElementById("ngay_lap").value,
+        tienCoc: document.getElementById("tien_coc").value,
+        trangThai: document.getElementById("trang_thai").value,
+        maKH: document.getElementById("ma_kh").value,
+        tenKH: document.getElementById("ten_kh").value,
+        diaChi: document.getElementById("dia_chi").value,
+        imei: document.getElementById("imei").value,
+        maNV: document.getElementById("ma_nv").value,
+        tenNV: document.getElementById("ten_nv").value,
+        maHang: document.getElementById("ma_hang").value,
+        soLuong: document.getElementById("t_so_luong").value,
+        thanhTien: document.getElementById("t_tien").value,
+        chietKhau: document.getElementById("t_ck").value,
+        thue: document.getElementById("t_thue").value,
+        diem: document.getElementById("t_diem").value,
+        tongPhaiThu: document.getElementById("t_phai_thu").value,
+    };
+
+    localStorage.setItem("saveData", JSON.stringify(dataToSave));
+
+    window.location.href = "../Hinh1/index.html";
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var editData = localStorage.getItem("editData");
+
+    if(editData){
+        var parseData = JSON.parse(editData);
+
+        document.getElementById("ngay_lap").value = parseData.ngayLap || "";
+        document.getElementById("so_phieu").value = parseData.soPhieu || "";
+        document.getElementById("ma_kh").value = parseData.makh || "";
+        document.getElementById("ten_kh").value = parseData.tenkh || "";
+        document.getElementById("t_tien").value = parseData.tongtien || "";
+        document.getElementById("t_ck").value = parseData.tongCk || "";
+        document.getElementById("t_thue").value = parseData.tongthue || "";
+        document.getElementById("t_phai_thu").value = parseData.tongphaithu || "";
+        document.getElementById("trang_thai").value = parseData.trangthai || "";
+    }
+});
+
 
